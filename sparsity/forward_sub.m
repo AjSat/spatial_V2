0db6b54ada5c0d7e6196b4baa8fb2b8bc5ce72fc
Ajay Sathya <sajaysathya@gmail.com>
2022-10-22 23:21:04 +0200

    minor

--- sparsity/forward_sub.m
+++ sparsity/forward_sub.m
@@ -1,5 +1,19 @@
 function [y] = forward_sub(L,w)
 
+%Function to find the Cholesky factor of a Positive Definite matrix A
+%Author Mathuranathan for https://www.gaussianwaves.com
+%Licensed under Creative Commons: CC-NC-BY-SA 3.0
+%A = positive definite matrix
+%Option can be one of the following 'Lower','Upper'
+%L = Cholesky factorizaton of A such that A=LL^T
+%If option ='Lower', then it returns the Cholesky factored matrix L in
+%lower triangular form
+%If option ='Upper', then it returns the Cholesky factored matrix L in
+%upper triangular form
+
+%Test for positive definiteness (symmetricity need to satisfy)
+%Check if the matrix is symmetric
+
 import casadi.*
 reg = 1e-12;
 
